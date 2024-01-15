339929bd7fc6c3b20335386ed998df2f6794fcc0
ISHIDA <ishida.ayumu@isoroot.jp>
2024-01-15 17:59:17 +0900

    デリゲートメソットで画面遷移

---
+++ StudyLayout/HomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HomeView.swift
+//  StudyLayout
+//
+//  Created by ishida.a on 2024/01/10.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, TransitionButtonCustomViewDelegate {
+    @IBOutlet weak var customView: TransitionButtonCustomView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 
+        customView.delegate = self
+
+    }
+    
+    /// 画面遷移処理
+    func didTapButton(){
+        let storyboard = UIStoryboard(name: "NexViewController", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "NexViewControllerID")
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+}
