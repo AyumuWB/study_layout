d57a8c7f2db1012ae40e001e5e3a02831e992f86
ISHIDA <ishida.ayumu@isoroot.jp>
2024-01-25 12:05:11 +0900

    コード整形とセル表示のコメント追加

--- StudyLayout/HomeTableView.swift
+++ StudyLayout/HomeTableView.swift
@@ -13,14 +13,17 @@ class HomeTableView: UIViewController,UITableViewDelegate, UITableViewDataSource
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Cellの登録
+        // cellの登録
         tableView.register(UINib(nibName: "HomeCustomTableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
-        }
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
     
+    /// cellの生成
+    /// Returns:カスタムcell
+    /// Returns:カスタムcellが作れなかった場合、デフォルトcell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as? HomeCustomTableViewCell{
             cell.setup(text: "swift")
@@ -29,6 +32,6 @@ class HomeTableView: UIViewController,UITableViewDelegate, UITableViewDataSource
         
         return UITableViewCell()
     }
-    }
-    
+}
+
 
