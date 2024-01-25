1c85f9dbd82fe9bb03d10357d45345eaab4a7cdd
ISHIDA <ishida.ayumu@isoroot.jp>
2024-01-25 12:06:39 +0900

    コード整形

--- StudyLayout/CustomView/HomeCustomTableViewCell.swift
+++ StudyLayout/CustomView/HomeCustomTableViewCell.swift
@@ -14,10 +14,10 @@ class HomeCustomTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
